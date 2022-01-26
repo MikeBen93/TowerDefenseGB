1d4e2c9c08b4d43371428e1bf65b76d7d5932d68
MikeBen93 <m.benkov93@gmail.com>
2022-01-26 19:11:08 +0300

    added simple wave spawner

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+    public Text waveCountdownText;
+
+    public float timeBetweenWaves = 4f;
+    private float _countdown = 2f;
+
+    private int _waveIndex = 0;
+
+    private void Update()
+    {
+        if (_countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            _countdown = timeBetweenWaves;
+        }
+
+        _countdown -= Time.deltaTime;
+
+        waveCountdownText.text = System.Math.Round(_countdown,1).ToString();
+    }
+
+    IEnumerator SpawnWave()
+    {
+        _waveIndex++;
+        Debug.Log($"WAVE INCOMING {_waveIndex}");
+        for (int i = 0; i < _waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        
+        
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, transform.rotation);
+    }
+
+}
